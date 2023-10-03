7773b1febef43990517d72db006ba58ea47d650b
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2023-10-03 16:09:38 +0530

    Add files via upload

---
+++ frequency_of_string.java
@@ -0,0 +1,32 @@
+
+package javalab;
+
+import java.util.Scanner;
+
+public class frequency_of_string {
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the String: ");
+			String input=sc.next();
+			char letter;
+			System.out.println("Enter the letter to check frequency: ");
+			letter=sc.next().charAt(0);
+			int length=input.length(),count=0,i=0;
+			char []charArray=input.toCharArray();
+			while(length>0) {
+				if(charArray[i]==letter) {
+					count++;
+				}
+				i++;
+				length--;
+			}
+			if(count>0) {
+			    System.out.println("Frequency of  the String is:"+count);	
+			}
+			else {
+				System.out.println("Character is not present");
+			}
+			sc.close();
+			}
+		
+}
---
+++ palindrome.java
@@ -0,0 +1,40 @@
+/**********************************************************************************************************************************************************
+ * File							:		Palindrome.java
+ * Description					: 		To check whether a string is palindrome or not
+ * Author 						:		Jibin Gigi
+ * Version						:		1.0
+ * Date							:		03/10/23
+ **************************************************************************************************************************************************************/
+
+import java.util.Scanner;
+
+public class palindrome {
+      public static void main(String[] args) {
+    	  Scanner sc = new Scanner(System.in);
+    	  System.out.println("Enter the String");
+    	  String input = sc.next();
+    	  boolean isPal = isPalindrome(input);
+    	  if(isPal) {
+    		  System.out.println("The Given String is Palidrome");
+    	  }
+    	  else {
+    		  System.out.println("The Given String is Not Palidrome");
+    	  }
+    	 
+    	    
+    	  
+    	  
+      }
+      static boolean isPalindrome(String input) {
+    	  int length = input.length();
+    	  char [] charArray=input.toCharArray();
+    	  for(int i=0;i<=length/2;i++) {
+    		  if(charArray[i]!=charArray[length-i-1]) {
+    			  return false;
+    		  }
+    	  }
+    	  return true;
+    	  
+      }
+
+}
