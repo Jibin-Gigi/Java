9e6fd3fdba82063846ea68541a20c62ef2bc27ca
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2023-10-10 00:56:16 +0530

    Add files via upload
    Java Program to find frequency of a character in a String

---
+++ charFrequency.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class charFrequency {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the string");
+        String sentence=new String();
+        sentence=scan.nextLine();
+        System.out.println("Enter the character to be searched");
+        char checkMe=scan.next().charAt(0);
+        int charCount=checkFrequency(sentence,checkMe);
+        System.out.println("No. of character occurence in String = "+charCount);
+    
+    }
+    
+    static int checkFrequency(String sentence,char checkMe){
+        char [] array=sentence.toCharArray();
+        int length=sentence.length(),charCount=0;
+        for(int i=0;i<length;i++){
+            if(array[i]==checkMe){
+                charCount++;
+            }
+        }
+      return charCount;  
+    }
+}
