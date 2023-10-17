e9c2290db3c5db9c7aa9e4a600f4ff02d0dfe970
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2023-10-17 16:29:01 +0530

    Update Addition_of_Positive_Numbers_Using_do_while.java

--- Addition_of_Positive_Numbers_Using_do_while.java
+++ Addition_of_Positive_Numbers_Using_do_while.java
@@ -1,10 +1,10 @@
-/****************************************************************************************************************************************************
- * File				:	Addition_of_positive_numbers.java
- * Description      :   Adding positive numbers and when a negative number is prompted,Programs shows sum of positive numbers using do while
- * Author           :   Jibin Gigi
- * Version          :   1.0
- * Date             :   26/09/23
-***************************************************************************************************************************************************/
+/****************************************************************************************************************************************************************************
+ * File		                        :	Addition_of_positive_numbers.java
+ * Description                          :   Adding positive numbers and when a negative number is prompted,Programs shows sum of positive numbers using do while
+ * Author                               :   Jibin Gigi
+ * Version                              :   1.0
+ * Date                                 :   26/09/23
+****************************************************************************************************************************************************************************/
 import java.util.Scanner;
 
 public class Addition_of_Positive_Numbers_Using_do_while {
