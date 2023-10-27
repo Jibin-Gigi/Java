7c758dee4db4b55537e315acc76f1268644e8d4c
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2023-10-27 15:00:32 +0530

    Add files via upload

---
+++ abstractClass.java
@@ -0,0 +1,40 @@
+
+public class abstractClass {
+	public static void main(String [] args) {
+	Rectangle rectangle=new Rectangle();
+	rectangle.numberOfSides();
+	Triangle triangle=new Triangle();
+	triangle.numberOfSides();
+	Hexagon hexagon=new Hexagon();
+	hexagon.numberOfSides();
+	
+	
+	}
+}
+abstract class Shapes{
+	int sides;	
+	abstract void numberOfSides() ;
+		
+	
+}
+
+class Rectangle extends Shapes{
+	void numberOfSides(){
+		System.out.println("No. of sides of Rectangle:4");
+		
+	}
+}
+
+class Triangle extends Shapes {
+	void numberOfSides(){
+	  System.out.println("Enter the no. of sides of Triangle:3 ");
+}
+}	
+	
+
+class Hexagon extends Shapes{
+	void numberOfSides(){
+		   System.out.println("Enter the no. of sides of Hexagon: 6");
+
+}
+}
