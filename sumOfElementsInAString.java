2d300b4b7d59eb32ae5a9eedce174ebaf1c2cdc8
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2023-11-28 09:49:42 +0530

    Add files via upload

---
+++ sumOfElementsInAString.java
@@ -0,0 +1,23 @@
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+public class sumOfElementsInAString {
+	
+	public static void main(String [] args) {
+		int sum=0;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the Number string ");
+		StringTokenizer  object=new StringTokenizer(sc.nextLine());
+		while(object.hasMoreElements()) {
+			int data=Integer.parseInt(object.nextToken());
+			sum=sum+data;
+		}
+		System.out.println("The sum of numbers in string="+sum);
+		
+		sc.close();
+		
+		}
+	}
+
+
+ 
