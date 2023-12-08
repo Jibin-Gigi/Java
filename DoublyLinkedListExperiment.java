357c84f3bfec675dd0cef8ec8b0bc83568d840b2
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2023-12-08 11:38:45 +0530

    Update DoublyLinkedListExperiment.java

--- DoublyLinkedListExperiment.java
+++ DoublyLinkedListExperiment.java
@@ -1,88 +1,97 @@
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.lang.IndexOutOfBoundsException;
 
 public class DoublyLinkedListExperiment {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		DoublyLInkedList<Integer>doublyLInkedList = new DoublyLInkedList<>();
-		boolean condition = true;
+		int choice,element,index,flag=0;
+		DoublyLinkedList<Integer> list= new DoublyLinkedList<>();
+		Scanner sc=new Scanner(System.in);
+		boolean condition=true;
 		while(condition) {
-			System.out.println("Enter your choice\n"
-			+"1.Insertion At Front\n"
-			+ "2.Insertion At End\n"
-			+"3.Insertion At any position\n"
-			+"4.Deletion at any Position\n"
-			+"5. Display\n"
-			+"6.Exit\n");
-		int choice = sc.nextInt();
-		int element,position;
+		System.out.println("Enter the choice\n"+"1.Insert at Beginning\n"+"2.Insert At End\n"+"3.Insert at any Position\n"+"4.Delete at any Position\n"
+							+"5.Display\n"+"6.Exit");
+		choice=sc.nextInt();
 		switch(choice) {
 		case 1:
-			System.out.println("Enter the element:");
-			 element = sc.nextInt();
-			doublyLInkedList.insertionAtFront(element);
+			System.out.println("Enter the element");
+			element=sc.nextInt();
+			list.InsertionAtBeginning(element);
+			flag++;
 			break;
-		
 		case 2:
-			System.out.println("Enter the element:");
-			 element = sc.nextInt();
-			doublyLInkedList.insertionAtEnd(element);
-			break;	
-		
+			System.out.println("Enter the element");
+			element=sc.nextInt();
+			list.InsertionAtEnd(element);
+			flag++;
+			break;
 		case 3:
-			System.out.println("Enter the element:");
-			 element = sc.nextInt();
-			System.out.println("Enter the position:");
-			 position = sc.nextInt();
-			doublyLInkedList.insertionAtAnyPosition(position,element);
+			try {
+			System.out.println("Enter the position at which element is to be added");
+			index=sc.nextInt();
+			System.out.println("Enter the element");
+			element=sc.nextInt();
+			list.InsertionAtAnyPosition(index, element);
+			flag++;
+			}catch (IndexOutOfBoundsException e) {
+				System.out.println("The given index is not present");
+			}
+			
 			break;
 		case 4:
-			
-			System.out.println("Enter the position:");
-			 position = sc.nextInt();
-			doublyLInkedList.removeAtAnyPosition(position);
+			try {
+			System.out.println("Enter the position at which element is to be deleted");
+			index=sc.nextInt();
+			list.DeleteAtAnyPosition(index);
+			flag--;
+			}catch(IndexOutOfBoundsException e) {
+				System.out.println("Given index is not present");
+			}
 			break;
-			
 		case 5:
-			System.out.println("The elements are :");
-			doublyLInkedList.display();
+			if(flag>0) {
+			list.display();
+			}else {
+				System.out.println("The LinkedList is empty");
+			}
 			break;
-		
 		case 6:
-			condition = false;
+			condition=false;
 			break;
-		default :
-			System.out.println("Invalid choice");
-			
-			 
-			
 		}
 		}
+		
 		sc.close();
 	}
+		
+}	
 
+class DoublyLinkedList<T>{
+	private LinkedList<T> list =new LinkedList<>();
 	
-}
-
-
-class DoublyLInkedList<T>{
-	private LinkedList<T>list = new LinkedList<>();
-	public void insertionAtEnd(T element) {
-		list.addLast(element);
-	}
-	public void insertionAtFront (T element) {
+	public void InsertionAtBeginning(T element) {
 		list.addFirst(element);
 	}
-	public void insertionAtAnyPosition (int index, T element) {
+	public void InsertionAtEnd(T element) {
+		list.addLast(element);
+	}
+	public void InsertionAtAnyPosition(int index,T element) {
 		list.add(index,element);
 	}
-	public void removeAtAnyPosition(int index) {
+	public void DeleteAtAnyPosition(int index) {
 		list.remove(index);
+		display();
 	}
 	public void display() {
+		System.out.println("The elements are: ");
 		for(T element : list) {
-			System.out.println(element +" ");
+			System.out.print(element +"  ");
 		}
 		System.out.println( );
 	}
 }
+
+
+
+
+
