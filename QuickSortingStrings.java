65670c78e1f6fa05324dc86b2cbe4e11587e0875
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2023-12-18 01:12:11 +0530

    Update QuickSortingStrings.java

--- QuickSortingStrings.java
+++ QuickSortingStrings.java
@@ -9,56 +9,75 @@
 import java.util.Scanner;
 
 public class QuickSortingStrings {
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // Input: Number of strings to be sorted
         System.out.print("Enter the limit: ");
         int limit = sc.nextInt();
         sc.nextLine(); 
 
+        // Array to store input strings
         String[] strings = new String[limit];
+
+        // Input: Strings to be sorted
         System.out.println("Enter the strings to be sorted:");
         for (int i = 0; i < limit; i++) {
             strings[i] = sc.nextLine();
         }
 
+        // Sorting using QuickSort algorithm
         quickSort(strings, 0, limit - 1);
 
+        // Output: Sorted strings
         System.out.println("Sorted strings:");
         for (int i = 0; i < limit; i++) {
             System.out.println(strings[i]);
-            sc.close();
+        }
+
+        sc.close(); // Close the Scanner
+    }
+
+    // Recursive method to perform QuickSort on an array of strings
+    public static void quickSort(String[] arr, int low, int high) {
+        if (low < high) {
+            // Partition the array and get the pivot index
+            int pi = partition(arr, low, high);
+
+            // Recursively sort the sub-arrays
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
         }
     }
-	
-	 public static void quickSort(String[] arr, int low, int high) {
-	        if (low < high) {
-	            int pi = partition(arr, low, high);
-	            quickSort(arr, low, pi - 1);
-	            quickSort(arr, pi + 1, high);
-	        }
-	    }
-	 
-	 public static int partition(String[] arr, int low, int high) {
-	        String pivot = arr[low]; // Select the first element as the pivot
-	        int i = low;
-	        for (int j = low + 1; j <= high; j++) {
-	            if (arr[j].compareTo(pivot) < 0) {
-	                i++;
-	                swap(arr, i, j);
-	            }
-	        }
-	        swap(arr, low, i); // Swap the pivot with the element at i
-	        return i;
-	    }
-	 
-     public static void swap(String[] arr, int i, int j) {
-	        String temp = arr[i];
-	        arr[i] = arr[j];
-	        arr[j] = temp;
-	    }
-	    
-}
 
+    // Helper method to partition the array and return the pivot index
+    public static int partition(String[] arr, int low, int high) {
+        // Select the first element as the pivot
+        String pivot = arr[low];
+        int i = low;
+
+        // Iterate through the array to rearrange elements around the pivot
+        for (int j = low + 1; j <= high; j++) {
+            if (arr[j].compareTo(pivot) < 0) {
+                i++;
+                // Swap elements if the current element is less than the pivot
+                swap(arr, i, j);
+            }
+        }
+
+        // Swap the pivot with the element at index i
+        swap(arr, low, i);
+        return i; // Return the pivot index
+    }
+
+    // Helper method to swap elements in an array
+    public static void swap(String[] arr, int i, int j) {
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
 
 
 
